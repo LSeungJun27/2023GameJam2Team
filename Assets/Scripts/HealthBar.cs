2f6a28501d123bd0265706b74044b0db9d842828
Box5789 <pck279049@gmail.com>
2023-08-28 23:10:16 +0900

    에셋 추가

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private Camera _camera;
+    public Slider slider;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _camera = Camera.main;
+        
+    }
+    
+    private void LateUpdate()
+    {
+        slider.transform.position = _camera.WorldToScreenPoint(gameObject.transform.position+Vector3.up);
+    }
+}
