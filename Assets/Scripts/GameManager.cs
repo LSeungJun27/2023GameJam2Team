d4e757fafce17ca3c921a7e82e4383878cc2ee69
LSeungJun27 <lsj927n@naver.com>
2023-08-30 14:24:59 +0900

    최고점수 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,10 @@ public class GameManager : MonoBehaviour
         public hpcount playerHpcount;
         public hpcount cartHpcount;
 
+    //점수
+    public Text BestScroetxt;
+    private int bestscore = 0;
+    private string KeyName = "Best";
     [SerializeField] public Text Scoretxt;
     private float YouScore;
     private int RealScore;
@@ -36,8 +40,15 @@ public class GameManager : MonoBehaviour
             {
                 Kill();
             });
-        }
         
+        //점수
+        bestscore = PlayerPrefs.GetInt(KeyName, 0);
+        BestScroetxt.text = bestscore.ToString();
+
+
+        }
+      
+    //점수
     public void Kill()
     {
         KillScore += killbonus;
@@ -45,25 +56,33 @@ public class GameManager : MonoBehaviour
         //Debug.Log(KillScore);
     }
 
+    //점수
     public void ScoreCount()
     {
-        //점수
         YouScore += Time.deltaTime;
         int Ks = GetComponent<GameManager>().KillScore;
         RealScore = Mathf.FloorToInt(YouScore) + KillScore;
+        if (RealScore > bestscore)
+        {
+            PlayerPrefs.SetInt(KeyName, RealScore);
+            bestscore = RealScore;
+        }
         Scoretxt.text = "Score : " + RealScore.ToString();
+        BestScroetxt.text = bestscore.ToString();
     }
         
 
         // Start is called before the first frame update
     void Start()
     {
+        //점수
             YouScore = 0;
     }
 
         // Update is called once per frame
     void Update()
     {
+        //점수
         ScoreCount();
     }
 } 
